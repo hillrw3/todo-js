a352850fcaa2926ed5c657c4acbd19a53074b04a
Rob Hill <robert.hill0122@gmail.com>
2014-08-26 16:45:45 -0600

    user sees flash message when adding todo item

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,17 +15,57 @@
 //= require_tree .
 
 $(document).ready(function () {
-  var body = $('body')
+  var body = $('body');
 
   var input = document.createElement('INPUT');
   input.setAttribute('type', 'text');
+  $(input).addClass('wide-input todo-input');
+
 
   var button = document.createElement('button');
   button.setAttribute('type', 'submit');
-  button.innerHTML = 'Create Todo'
+  button.innerHTML = 'Create Todo';
+  $(button).addClass('wide-input');
+  $(button).attr('id', 'create');
+
+  var list = document.createElement('ul');
+
+  var flash = document.createElement('div');
+  $(flash).addClass('flash wide-input');
+
+  var x = "<button>X</button>";
+  $(flash).append("<p>Todo created </p>");
+  $(flash).append(x);
+
 
   body.append("<h1>Todo.ly</h1>").addClass('centered-text');
   body.append(input);
   body.append('<br/>');
   body.append(button);
+  body.append('<br/>');
+  body.append('<br/>');
+  body.append('<hr>');
+  body.append("<h1>Todo!</h1>");
+  body.append(flash);
+  body.append('<br/>');
+  body.append(list);
+
+  $('#create').on('click', function() {
+    var newTodo = "<li>" + $('input').val() +  x + "</li>";
+    $(list).append(newTodo).addClass('wide-input float-left');
+    $('input').val('');
+    $(flash).show();
+    setTimeout("$('.flash').fadeOut();", 5000);
+  });
+
+  $('.flash button').on('click', function(){
+    $('.flash').hide();
+  });
+
+  $('li button').on('click', function() {
+    $(parent).remove();
+    alert("you clicked the damn button!");
+  });
+
+
 });
